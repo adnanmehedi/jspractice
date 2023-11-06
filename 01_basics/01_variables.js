dbbd2e44bac7d903390d19dbd9864e8547cb2c7b
adnanmehedi <69539815+adnanmehedi@users.noreply.github.com>
2023-11-06 21:32:58 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = 'adnanmehedi123@gmail.com'
+var accountPassword = "12345"
+//accountCity = "Jaipur"
+accountPassword = 25
+let accountState
+console.table([accountEmail, accountId, accountPassword , accountState])
+
+/* Prefer not to use var because of issue in block scope and functional scope */
